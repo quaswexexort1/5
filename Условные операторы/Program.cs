3b11d961eafb335c499c41d4b78eabb4c1bb9612
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-10-18 17:18:25 +0200

    Добавьте файлы проекта.

---
+++ Условные операторы/Program.cs
@@ -0,0 +1,28 @@
+﻿//Дано четырехзначное число. Определить: а) равна ли сумма двух первых его цифр сумме двух последних б) кратна ли 7 сумма его цифр.
+
+try
+{
+    Console.Write("Введите четырехзначное число:");
+    int a = int.Parse(Console.ReadLine());
+    
+    double a1 = a / 1000;
+    double a2 = (a % 1000) / 100;
+    double a3 = (a % 100) / 10;
+    double a4 = a % 10;
+    double firstsum = a1 + a2;
+    double secondsum = a3 + a4;
+    if (firstsum == secondsum) Console.WriteLine($"Суммы равны, {firstsum}={secondsum}");
+    else Console.WriteLine($"Суммы не равны; {firstsum} не равно {secondsum}");
+    int sum = 0;
+    sum += a / 1000; //Первая цифра
+    sum += (a / 100) % 10; //Вторая цифра
+    sum += (a / 10) % 10; //Третья цифра
+    sum += a % 10;  //Четвертая цифра
+    if (sum % 7 == 0) Console.WriteLine("Сумма цифр числа кратна 7");
+    else Console.WriteLine("Сумма цифр числа не кратна 7");
+
+}
+catch
+{
+    Console.WriteLine("Введите данные правильно!");
+}
